8f4bb7c9ce8a15246d8fd571b89cb3b7947a7bf2
Kashish Bhandari <kashishbhandari09@gmail.com>
2026-04-18 22:16:15 -0500

    Update auth.py

--- Info/auth.py
+++ Info/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
-from models import get_db
+from Info.models import get_db
 
 auth_bp = Blueprint("auth", __name__)
 
